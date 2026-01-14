9ada32ca05e15936aa217f48139bff94941ab01d
Vikas Jha <jhavikas2004@gmail.com>
2026-01-14 12:56:27 +0530

    Added dependency `spring-boot-starter-test` for Testing; Created unit tests for the algorithms

---
+++ src/test/java/com/vikas/rate_limiter/LeakyBucketTest.java
@@ -0,0 +1,12 @@
+package com.vikas.rate_limiter;
+
+public class LeakyBucketTest {
+
+	// @Test
+	// void onlyLimitedTestPassEachSecond() {
+	// }
+	//
+	// @Test
+	// void requestDroppedAfterFullBucket() {
+	// }
+}
